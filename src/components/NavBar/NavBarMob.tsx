67601ce886bf9ee51e039151be8e71f9b7ac3e80
Кирилл <91612813+jiuper@users.noreply.github.com>
2024-11-01 17:27:45 +0300

    fix

---
+++ src/components/NavBar/NavBarMob.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useRef, useState } from "react";
+import cnBind from "classnames/bind";
+import Link from "next/link";
+
+import { items } from "@/components/NavBar/constants";
+import { ArrowDownIcon } from "@/shared/assests/svg/svg";
+
+import styles from "./Navbar.module.scss";
+
+const cx = cnBind.bind(styles);
+export const NavBarMob = ({
+    className,
+    classNameItems,
+    onClick,
+}: {
+    className?: string;
+    classNameItems?: string;
+    onClick?: () => void;
+}) => {
+    const ref = useRef<HTMLDivElement>(null);
+
+    const [index, setIndex] = useState<number | null>(null);
+    const handleClick = (i: number) => {
+        setIndex(i === index ? null : i);
+        onClick?.();
+    };
+    const handleClickOutside = (event: MouseEvent) => {
+        if (ref.current && !ref.current.contains(event.target as Node)) {
+            setIndex(null);
+        }
+    };
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <div className={cx("navbar-mob", className)} ref={ref}>
+            <div className={cx("items-mob", classNameItems)}>
+                {items.map((item, i) => (
+                    <div key={item.label} className={cx("item-mob")}>
+                        {item.url ? (
+                            <Link
+                                className={cx("link-mob", "link-icon-mob")}
+                                onClick={onClick}
+                                href={item.url}
+                                key={item.label}
+                            >
+                                {item.label}
+                                <ArrowDownIcon className={cx("icon-mob")} />
+                            </Link>
+                        ) : (
+                            <div className={cx("link-mob", "submenu-mob")} onClick={() => handleClick(i)}>
+                                {item.label}
+                                <ArrowDownIcon className={cx("icon-mob")} />
+                            </div>
+                        )}
+                        {item.items && (
+                            <div className={cx("subitems-wrapper-mob", index === i && "open-mob")}>
+                                <div className={cx("subitems-mob", "active-sub-mob")}>
+                                    {item.items?.map((subitem) => (
+                                        <div key={subitem.label} className={cx("subitem-mob")}>
+                                            <Link className={cx("link-mob")} onClick={onClick} href={subitem.url}>
+                                                {subitem.label}
+                                            </Link>
+                                            <ArrowDownIcon className={cx("icon-mob")} />
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
